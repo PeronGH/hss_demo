9b724df594b1c6269aa72554760f6509e828f874
peron <peron_ap@icloud.com>
2024-01-03 15:26:29 +0000

    feat: allow custom port

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import "std/dotenv/load.ts";
 import { app } from "./src/app.tsx";
+import { maybeParseInt } from "./src/utils.ts";
 
-Deno.serve(app.fetch);
+Deno.serve({ port: maybeParseInt(Deno.env.get("PORT")) }, app.fetch);
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,17 @@ export type MaybePromise<T> = T | Promise<T>;
 export function toPercents(value: number): string {
   return `${Math.round(value * 100)}%`;
 }
+
+export function maybeParseInt(value: unknown): number | undefined {
+  const valueType = typeof value;
+  if (valueType === "number") {
+    return value as number;
+  }
+  if (valueType === "string") {
+    const parsed = parseInt(value as string, 10);
+    if (!isNaN(parsed)) {
+      return parsed;
+    }
+  }
+  return undefined;
+}
